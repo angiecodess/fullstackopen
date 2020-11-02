2df5ae94a449342941e1dcf558a6c244b707673c
Ann Wong <angie_wong3345@hotmail.com>
2020-11-01 21:11:25 -0500

    heroku added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
+
 
 let notes = [
     {
@@ -82,7 +84,7 @@ app.get('/api/notes', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
